078bfc1a6117341fc7355c4c971825eaf78ba497
Gabriel <gabrielmoyagc@gmail.com>
2026-06-24 18:58:41 +0200

    Estructura de datos: Listas

---
+++ 05-Listas.py
@@ -0,0 +1,15 @@
+
+# * Las listas son estructuras de datos que pueden contener elementos de diferentes tipos.
+# * Se definen utilizando corchetes [] y los elementos se separan por comas.
+# * Las listas son mutables, lo que significa que puedes modificar su contenido después de haberlas creado.
+
+numeros = [1, 2, 3]
+letras = ['a', 'b', 'c']
+palabras = ['hola', 'mundo']
+booleanos = [True, False, True]
+matriz = [[1, 2], [3, 4]]
+ceros = [10] * 5
+alfanumericos = numeros + letras
+rangos = list(range(1, 11))
+caracteres = list('Python')
+    
