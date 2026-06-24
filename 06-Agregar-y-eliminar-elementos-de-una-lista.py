f4f69185b21c427a8ea5d166060d1ef36d52417f
Gabriel <gabrielmoyagc@gmail.com>
2026-06-24 18:59:09 +0200

    Métodos para agregar y eliminar elementos en listas

---
+++ 06-Agregar-y-eliminar-elementos-de-una-lista.py
@@ -0,0 +1,16 @@
+juegos = [
+    "FIFA", 
+    "Call of Duty", 
+    "Minecraft", 
+    "Fortnite",
+    "FIFA"
+]
+# * Agregar un elemento a la lista
+#  juegos.insert(2, "Among Us")  # Agrega "Among Us" en la posición 2
+#  juegos.append("Valorant")  # Agrega "Valorant" al final de la lista
+#  juegos.remove("FIFA")  # Elimina la primera aparición de "FIFA"
+#  juegos.pop(1)  # Elimina el elemento en la posición 1 (Call of Duty)
+#  del juegos[2]  # Elimina el elemento en la posición 2 (Minecraft)
+#  juegos.clear()  # Elimina todos los elementos de la lista
+print(juegos)
+    
