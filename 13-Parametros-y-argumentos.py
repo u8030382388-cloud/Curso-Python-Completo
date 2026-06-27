70e6a03b591493a13614b285478aad4a4dae07d5
Gabriel <gabrielmoyagc@gmail.com>
2026-06-27 17:19:13 +0200

    Parámetros y argumentos de función

--- 13-Parametros-y-argumentos.py
+++ 13-Parametros-y-argumentos.py
@@ -1,11 +1,14 @@
 """Ejemplo de función de saludo con valores por defecto."""
 
 # ! Parámetros
+# es una variable que recibe la función al ser definida
+
 def saludo(nombre: str = "Hola", apellido: str = "Mundo") -> str:
     """Devuelve un saludo usando nombre y apellido."""
     return f"Hola {nombre} {apellido}"
 
 # ? Argumentos
+# es el valor que se le pasa a la función al ser llamada
 print(saludo("Gabriel", "Moya"))
 print(saludo("Gabriel"))
 print(saludo())
